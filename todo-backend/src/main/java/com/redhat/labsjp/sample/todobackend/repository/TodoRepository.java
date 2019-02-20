736dfe56768a0fc00ed3e2a1f84209dd451eee46
Yosuke Tsuboi <ytsuboi@redhat.com>
2019-02-20 15:58:02 +0900

    scaffolded

--- todo-backend/src/main/java/com/redhat/labsjp/sample/todobackend/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.labsjp.sample.todobackend.repository;
-
-import com.redhat.labsjp.sample.todobackend.domain.Todo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TodoRepository extends CrudRepository<Todo, Long> {
-    
-}
--- todo-frontend/src/router.js
+++ todo-frontend/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Todo from '@/components/Todo'
 
 Vue.use(Router)
 
@@ -21,11 +20,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/todo',
-      name: 'todo',
-      component: Todo
     }
   ]
 })
